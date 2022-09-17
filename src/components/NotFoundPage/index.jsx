7038b2a3503a4e567e5d7a97c4ac38aa122bae41
Vitor <vitor.mancio@ggmail.com>
2022-09-17 11:17:14 -0300

    create a 404-page

---
+++ src/components/NotFoundPage/index.jsx
@@ -0,0 +1,9 @@
+import './styles.scss'
+
+export function NotFoundPage () {
+    return (
+        <div className='wrapPageNotFound'>
+        <h1>Erro 404 - Página não encontrada</h1>
+        </div>
+    )
+}
